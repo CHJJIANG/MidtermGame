08207697ad38a215a0f53ee51ec707c5def19085
CHJJIANG <bob2277645424@gmail.com>
2023-03-26 19:48:07 -0400

    ok

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private int Boxnum = 15;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +21,6 @@ public class Enemy : MonoBehaviour
     void destoryobject()
     {
         Destroy(gameObject);
+        Boxnum -= 1;
     }
 }
